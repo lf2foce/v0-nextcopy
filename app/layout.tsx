e48ac6451b521a7faaa22a78be78a711dfed0bf6
Anh Bui <lf2foce@gmail.com>
2025-05-01 14:46:45 +0700

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css"
 import { ToastProvider } from "@/components/toast-provider"
 import { SidebarProvider } from "@/components/ui/sidebar"
 import Sidebar from "@/components/sidebar"
+import { Analytics } from "@vercel/analytics/react"
 
 import {
   ClerkProvider
@@ -33,7 +34,10 @@ export default function RootLayout({
           <SidebarProvider>
             <div className="flex min-h-screen">
               <Sidebar />
-              <main className="flex-1 p-4 md:p-6 lg:p-10 bg-yellow-50 w-full overflow-x-hidden">{children}</main>
+              <main className="flex-1 p-4 md:p-6 lg:p-10 bg-yellow-50 w-full overflow-x-hidden">
+                {children}
+                <Analytics />
+                </main>
             </div>
           </SidebarProvider>
         </ToastProvider>
